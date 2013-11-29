f8eabab70a8fe46abf23effaf056dd23390f8166
jnorthr <james.b.northrop@googlemail.com>
2013-11-29 13:24:51 +0100

    fix song chooser

--- src/main/groovy/javazoom/jl/player/MP3.groovy
+++ src/main/groovy/javazoom/jl/player/MP3.groovy
@@ -22,15 +22,17 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 
 import javazoom.jl.player.Player;
-
+import javazoom.jl.player.SongChooser;
 
 public class MP3 {
     private String filename;
     private Player player; 
     int playtime = 10; // default to 10 sec.s of play
+    boolean gui = false;
+    def songs = []
 
     // constructor that takes the name of an MP3 file
-    public MP3(String filename) {
+    public setName(String filename) {
         this.filename = filename;
     }
 
@@ -70,32 +72,114 @@ public class MP3 {
 
     } // end of play
 
+
+    // figure out the command line options or if the parm is a valid .mp3 song that exists, put in into the songs list
+    public void decodeArgs(args)
+    {
+	args.each{a->
+		def at = a.trim()
+		def atlc = at.toLowerCase();
+		def flag = (atlc.startsWith('-')) ? true : false;
+
+		if (flag)
+		{
+			switch(atlc)
+			{
+				case "-gui" : gui = true;
+				break;
+			} // end of switch
+		}
+		else
+		{
+			if (new File(at).exists() && (atlc.endsWith(".mp3")) )
+			{
+				this.songs += at;
+			} // end of if
+			else
+			{
+				if (at.isInteger())
+				{
+					try{ this.playtime = at as int; } catch(Exception x){ this.playtime = 15; }
+					if ( this.playtime < 1 ) { this.playtime = 15; }
+				} // end of if
+				else
+				{
+					println "this is an unknown song, is not an mp3 file and is not a valid command option:"+a;
+				} // end of else
+
+			} // end of else
+
+		} // end of if
+
+	} // end of each
+
+	// user asked to choose a song
+	if (gui)
+	{
+		pickSong();
+	}// end of if
+
+    } // end of decode
+
+
+    // =========================
+    // song chooser
+    def pickSong()
+    {
+		def sc = new SongChooser();
+		// will return null or valid .mp3 filename that exists and is not a directory
+        	def song = sc.getSong()
+		if (song!=null)
+		{
+			this.songs += song.toString();
+                 	println "you chose "+song.toString()
+		} // end of if
+    } // end of pickSong
+
+
+
     // play client
     public static void main(String[] args) {
-    
-        String filename = (args.size() > 0) ? args[0] : "resources/Love.mp3";
-        MP3 mp3 = new MP3(filename);
-
-        String playtimes = (args.size() > 1) ? args[1] : "10";
-	try{ mp3.playtime = playtimes as int; } catch(Exception x){ mp3.playtime = 10; }
-
-	def th = Thread.start 
+        MP3 mp3 = new MP3();
+    	
+	if (args.size() > 0)
 	{
-		println 'starting to play '+filename
-	        mp3.play();
-    		sleep mp3.playtime * 1000;
-    		//throw new NullPointerException()
-	} // end of thread start
+		mp3.decodeArgs(args);
+	} // end of if
+	else
+	{
+		mp3.pickSong();
+	} // end of else
 
-	th.setDefaultUncaughtExceptionHandler({t,ex ->
-    		println 'ignoring: ' + ex.class.name
-	} as Thread.UncaughtExceptionHandler)
+	if (mp3.songs.size() < 1)
+	{	
+		mp3.songs += "resources/ring.mp3";
+	} // end of if
 
-	th.join()
 
-        // when  done, stop playing it
-        mp3.close();
-    }
+	mp3.songs.each{song->
+	        String filename = song;
+        	mp3.setName(filename);
 
-}
+		def th = Thread.start 
+		{
+			println 'starting to play '+filename+" for "+mp3.playtime+" sec.s"
+	        	mp3.play();
+    			sleep mp3.playtime * 1000;
+    			//throw new NullPointerException()
+		} // end of thread start
+
+		th.setDefaultUncaughtExceptionHandler({t,ex ->
+    			println 'ignoring: ' + ex.class.name
+		} as Thread.UncaughtExceptionHandler)
+
+		th.join()
+
+	        // when  done, stop playing it
+        	mp3.close();
+	} // end of each
+
+    } // end of main
+
+} // end of class
 
